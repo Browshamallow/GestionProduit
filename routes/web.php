dca6e27487d1e7c239006a911ba0aa4cb4e309be
Brownshamallow <thiombanendoumbe02@gmail.com>
2025-07-10 14:18:28 +0200

    routes web

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 
+
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\ProfileController;
@@ -18,6 +19,7 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
 Route::middleware(['auth'])->group(function () {
     Route::get('/produits', [ProductController::class, 'index'])->name('produits.index');
     Route::get('/produits/create', [ProductController::class, 'create'])->name('produits.create');
